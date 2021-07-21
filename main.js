773c96c01b753a9c3f9387716ad4383d3738996a
zdresser <42179244+zdresser@users.noreply.github.com>
2021-07-21 16:10:14 -0400

    init

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-// your code here
+document.getElementById('submit').addEventListener('click', function() {
+
+  
+  var name = document.getElementById('name').value;
+  var message = document.getElementById('message').value;
+  
+
+  var postsDiv = document.querySelector('.posts');
+
+  var newPostsDiv = document.createElement('div');
+
+  var newPostMessageP = document.createElement('p');
+  var newPostMessageText = document.createTextNode(message);
+  newPostMessageP.appendChild(newPostMessageText);
+
+  var newPostNameP = document.createElement('p');
+  var newPostNameText = document.createTextNode('Posted by: ' + name);
+  newPostNameP.appendChild(newPostNameText);
+
+  var newPostHR = document.createElement('hr');
+
+  newPostsDiv.append(newPostMessageP);
+  newPostsDiv.append(newPostNameP);
+  newPostsDiv.append(newPostHR);
+  postsDiv.append(newPostsDiv);
+});
